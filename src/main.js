54e3f624efd822a27812dd02c97bb445cfb3e058
Lydia Bandy <121919536+LydiasPianoStudio@users.noreply.github.com>
2024-04-07 10:11:51 -0400

    Added own character
    updated to add my own character. need to make a map in Tile.

--- src/main.js
+++ src/main.js
@@ -6,21 +6,21 @@ k.loadSprite("spritesheet", "./spritesheet.png", {
   sliceX: 39,
   sliceY: 31,
   anims: {
-    "idle-down": 956,
-    "walk-down": { from: 956, to: 939, loop: true, speed: 8 },
-    "idle-side": 975,
-    "walk-side": { from: 975, to: 978, loop: true, speed: 8 },
-    "idle-up": 1014,
-    "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 },
+    "idle-down": 952,
+    "walk-down": { from: 952, to: 955, loop: true, speed: 8 },
+    "idle-side": 991,
+    "walk-side": { from: 991, to: 994, loop: true, speed: 8 },
+    "idle-up": 1030,
+    "walk-up": { from: 1030, to: 1033, loop: true, speed: 8 },
   },
 });
 
-k.loadSprite("map", "./map.png");
+k.loadSprite("map", "./map.png"); 
 
-k.setBackground(k.Color.fromHex("#311047"));
+k.setBackground(k.Color.fromHex("#0013de")); 
 
 k.scene("main", async () => {
-  const mapData = await (await fetch("./map.json")).json();
+  const mapData = await (await fetch("./map.json")).json(); 
   const layers = mapData.layers;
 
   const map = k.add([k.sprite("map"), k.pos(0), k.scale(scaleFactor)]);
