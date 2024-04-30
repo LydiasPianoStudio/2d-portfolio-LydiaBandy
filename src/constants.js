dd36d2a2bbe48741ca4d32970cf7db1c9477d77b
Lydia Bandy <121919536+LydiasPianoStudio@users.noreply.github.com>
2024-04-30 19:56:10 -0400

    Updates
    learning tiled and updating dialogueData

--- src/constants.js
+++ src/constants.js
@@ -8,9 +8,8 @@ export const dialogueData = {
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
   I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
   You'll learn how I built this portfolio you're currently playing through!`,
-  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  tv: `What is your favorite comedy show? Mine is The Office. I made a hangman game based on The Office. Check it out here:
+   <a href="https://lydiaspianostudio.github.io/Hangman-Game/" target="_blank">The_Office_HangmanGame</a>`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
   resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
   Contact me at LydiaBandy@gmail.com if you have any interesting job opportunities!`,
@@ -19,5 +18,5 @@ export const dialogueData = {
   Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
   library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
   I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+  exit: `Thanks for stopping by and visiting LydiasPianoStudio's portfolio, just close the tab.`,
 };
