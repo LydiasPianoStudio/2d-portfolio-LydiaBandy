a034e4afcc2bde2f41f405f9979ddf67f5e7f7dd
Lydia Bandy <121919536+LydiasPianoStudio@users.noreply.github.com>
2024-05-02 19:06:54 -0400

    Update constants.js
    adding more embeds and links to the dialogue

--- src/constants.js
+++ src/constants.js
@@ -6,19 +6,30 @@ export const dialogueData = {
   "cs-degree": `I attended McDaniel College receiving a B.A. in Music.
    Did you know coding and music are similar languages?🎹💻`,
   "sofa-table": `I like to relax on my couch and upload harp and piano videos on my youTube channel for the world to see🎹. 
-  Subscribe to <a href="https://youtube.com/@lydiaspianostudio" target="_blank">@Lydiaspianostudio</a>!!`,
+  Subscribe to <a href="https://youtube.com/@lydiaspianostudio" target="_blank">@Lydiaspianostudio</a>!!
+  I am a BIG gamer and LOVE Final Fantasy games.🎮 Here is a fun video I did for a FF contest in 2014😃💞:
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/OP11fdAAoJM?si=tc7VCVeIiFK0Xxj4" title="YouTube video player" 
+  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+  referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  `,
   tv: `What is your favorite comedy show? 
   <br> I like to chill at my TV and watch The Office. 
   <br> I made a hangman game based on The Office. 
   <br> Check it out here:
   <a href="https://lydiaspianostudio.github.io/Hangman-Game/" target="_blank">The_Office_HangmanGame</a>
  <br> <img src="./Hangman_Office_Edition.png" width="200px" height="160px" align="middle" alt="The_Office_HangmanGame" class="center" </img>`, 
-  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  bed: `I am not only a software engineer, but a music composer! 😃🎵🎹🎶 
+  Have trouble sleeping? Listen to my album, "Piano Pearls" so you can drift off to sleep.👶🏻
+  <iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/52xxPzgfdnhflFnTu5g0OY?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay;
+   clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe> 
+   <br> Follow my music on Spotify!`,
   resume: `Would you like to see my resume? <a href="https://github.com/LydiasPianoStudio/Resume/blob/main/LydiasPianoStudio_Resume.pdf" target="_blank">LydiasPianoStudio_Resume</a>
-  Contact me at LydiaBandy@gmail.com or connect with me at my <a href="https://linkedin.com/in/lydia-bandy-2b160745/ target="_blank">LinkedIn</a>!`,
+  Contact me at LydiaBandy@gmail.com or connect with me at my <a href="https://linkedin.com/in/lydia-bandy-2b160745/" target="_blank">LinkedIn</a>!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas..`,
   library: `.`,
-  exit: `Thanks for stopping by and visiting LydiasPianoStudio's portfolio, just close the tab.`,
+  exit: `Thanks for stopping by and visiting me! 
+   <a href="https://lydiaspianostudio.com/" target="_blank">LydiasPianoStudio</a>
+   , just close the tab and stop by anytime!.`,
 
 };
