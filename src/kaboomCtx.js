6bb2769fe8208701c96fdb0a51798af16e03a04a
Lydia Bandy <121919536+LydiasPianoStudio@users.noreply.github.com>
2025-04-28 16:35:10 -0400

    Update Kaboom import path

--- src/kaboomCtx.js
+++ src/kaboomCtx.js
@@ -1,5 +1,5 @@
-import kaboom from "kaboom"; // Use the aliased import // Use absolute path
-import { scaleFactor } from "./constants.js"; // Add .js extension
+import kaboom from "../node_modules/kaboom/dist/kaboom.mjs";
+import { scaleFactor } from "./constants.js";
 
 export const k = kaboom({
   global: false,
