38126616531fc30394391e58ff2a6e1f0ecc75a0
Lydia Bandy <121919536+LydiasPianoStudio@users.noreply.github.com>
2025-04-28 23:44:04 -0400

    Fix asset loading and base URL configuration

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { dialogueData, scaleFactor } from "./constants.js";
 import { k } from "./kaboomCtx.js";
 import { displayDialogue, setCamScale } from "./utils.js";
 
-const baseUrl = "/2d-portfolio-LydiaBandy/";
+// const baseUrl = "/2d-portfolio-LydiaBandy/";
+const baseUrl = import.meta.env.BASE_URL;
 
 // --- Preload Assets ---
 async function preloadAssets() {
